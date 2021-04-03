25890f1722097b6668b90bf8389025f8bf916e72
dolphius21 <johnrudolphlee@gmail.com>
2021-04-03 18:44:39 +0800

    done with sidebar modal projects

--- 12-sidebar-modal/src/context.js
+++ 12-sidebar-modal/src/context.js
@@ -1,1 +1,35 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from 'react';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        showModal,
+        showSidebar,
+        toggleModal,
+        toggleSidebar
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider, useGlobalContext };
