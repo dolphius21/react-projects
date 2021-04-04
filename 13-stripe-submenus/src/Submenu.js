aa7ac953bbe57ce05de72921546f167b3d8a9f1c
dolphius21 <johnrudolphlee@gmail.com>
2021-04-04 16:48:19 +0800

    done with stripe submenu project

--- 13-stripe-submenus/src/Submenu.js
+++ 13-stripe-submenus/src/Submenu.js
@@ -1,7 +1,39 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react';
+import { useGlobalContext } from './context';
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
-}
+  const {
+    showSubmenu,
+    location,
+    page: { page, links }
+  } = useGlobalContext();
+  const container = useRef(null);
+  const [columns, setColumns] = useState(2);
 
-export default Submenu
+  useEffect(() => {
+    const submenu = container.current;
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+    setColumns(links.length);
+  }, [location, links]);
+
+  return (
+    <aside className={`submenu ${showSubmenu && 'show'}`} ref={container}>
+      <h4>{page}</h4>
+      <div className={`submenu-center col-${columns}`}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link;
+          return (
+            <a key={index} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </aside>
+  );
+};
+
+export default Submenu;
